ad41fffebaf7e83294b2e07d145ac7c119be3852
Nick Callaghan <nicholas.callaghan@gmail.com>
2020-08-23 17:43:28 +0100

    fix booking lik

--- src/components/booking.js
+++ src/components/booking.js
@@ -1,16 +1,27 @@
 import React, { useState } from "react"
-import { withPrefix } from "gatsby"
+import { useStaticQuery } from "gatsby"
 import Button from "./button.js"
 
 const Booking = () => {
+  const data = useStaticQuery(graphql`
+    query {
+      site {
+        siteMetadata {
+          bookingLink
+        }
+      }
+    }
+  `)
+  const { bookingLink } = data.site.siteMetadata
   return (
     <section id="booking">
       <h2>Make a booking</h2>
-      <p>Reservations are available for groups of up to 8 people.</p>
-      <Button
-        variant="dark"
-        link="https://bookingninja.io/bookings/burger-restaurant"
-      >
+      <p>
+        Reservations are available for groups of up to 8 people. Due to social
+        distancing we are operating at a reduced capacity please let us know if
+        you no longer require your booking slot.
+      </p>
+      <Button variant="dark" link={bookingLink}>
         Book Now
       </Button>
     </section>
